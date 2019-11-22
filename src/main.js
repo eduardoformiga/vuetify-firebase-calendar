6a38827fb049bb0f7d29e3c835e27bbeec891a33
Eduardo Formiga <eduardo.formiga@gmail.com>
2019-11-22 00:00:09 -0300

    feat: vue calendar using vuetify and firebase

--- src/main.js
+++ src/main.js
@@ -1,8 +1,28 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import vuetify from "./plugins/vuetify";
+import VueTextareaAutosize from "vue-textarea-autosize";
+import firebase from "firebase/app";
+import "firebase/firestore";
 
-Vue.config.productionTip = false
+Vue.use(VueTextareaAutosize);
+
+Vue.config.productionTip = false;
+
+// Initialize Firebase
+firebase.initializeApp({
+  apiKey: "AIzaSyBYuAyCN9B-kq3HwvjH7clEMCrFWjFHT5I",
+  authDomain: "vuetify-firebase-calendar.firebaseapp.com",
+  databaseURL: "https://vuetify-firebase-calendar.firebaseio.com",
+  projectId: "vuetify-firebase-calendar",
+  storageBucket: "vuetify-firebase-calendar.appspot.com",
+  messagingSenderId: "228319444847",
+  appId: "1:228319444847:web:c8bdb496612aba2cb1729a"
+});
+
+export const db = firebase.firestore();
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  vuetify,
+  render: h => h(App)
+}).$mount("#app");
